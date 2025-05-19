e7042d1dc6253f655e0223878252cad93dafe812
ryan <ryanennns@gmail.com>
2025-05-19 14:37:35 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-3xl font-bold underline">Snickers</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
