6b0fa81b34e443f73baa89647a060beadc997225
Ryan Enns <ryanennns@gmail.com>
2025-05-22 16:02:31 -0400

    woops

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "snickers:3900";
-// export const API_BASE_URL = "https://blast-api.enns.dev";
+// export const API_BASE_URL = "snickers:3900";
+export const API_BASE_URL = "https://blast-api.enns.dev";
